1676d4070f3265e4465fdc8560919520015e3e23
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-08 18:01:11 +0300

    feat: space added

--- theme.ts
+++ theme.ts
@@ -4,7 +4,7 @@ const theme = extendTheme({
   colors: {
     pink: '#ff4cc1',
     cyan: '#38e0e0',
-    bgDark: ' #1a1a1a',
+    bgDark: '#1a1a1a',
     itemBgDark: '#262626',
     green: '#29d792',
     textDark: '#c9c9c9',
@@ -12,6 +12,17 @@ const theme = extendTheme({
     iconColor: '#a3a3a3',
     purple: '#4a293e',
     blueGreen: '#1e4544',
+  },
+  space: {
+    xxs: 2,
+    xs: 4,
+    s: 8,
+    sm: 12,
+    m: 16,
+    l: 24,
+    xl: 32,
+    xxl: 40,
+    xxxl: 48,
   },
 });
 
