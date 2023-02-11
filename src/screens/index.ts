f3bd720ad3040b705fdcec0ff9e648ae3187dbcf
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-11 15:33:40 +0300

    feat: index file created

---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+export {default as Feed} from './Feed';
+export {default as Account} from './Account';
+export {default as Login} from './Authentication/Login';
+export {default as Register} from './Authentication/Register';
+export {default as ResetPassword} from './Authentication/ResetPassword';
+export {default as Search} from './Search';
