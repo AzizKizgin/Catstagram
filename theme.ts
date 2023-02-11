96ad31c85e90e13c334809b38b841f230808d756
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-11 17:36:12 +0300

    refactor: code refactored

--- theme.ts
+++ theme.ts
@@ -1,17 +1,30 @@
 import {extendTheme} from 'native-base';
 
+const themeColors = {
+  pink: '#ff4cc1',
+  cyan: '#1f8686',
+  bgDark: '#1a1a1a',
+  itemBgDark: '#262626',
+  green: '#29d792',
+  textDark: '#c9c9c9',
+  inactiveTextDark: '#a2a2a2',
+  iconColor: '#a3a3a3',
+  purple: '#4a293e',
+  blueGreen: '#1e4544',
+};
+
 const theme = extendTheme({
   colors: {
-    pink: '#ff4cc1',
-    cyan: '#38e0e0',
-    bgDark: '#1a1a1a',
-    itemBgDark: '#262626',
-    green: '#29d792',
-    textDark: '#c9c9c9',
-    inactiveTextDark: '#a2a2a2',
-    iconColor: '#a3a3a3',
-    purple: '#4a293e',
-    blueGreen: '#1e4544',
+    pink: themeColors.pink,
+    cyan: themeColors.cyan,
+    bgDark: themeColors.bgDark,
+    itemBgDark: themeColors.itemBgDark,
+    green: themeColors.green,
+    textDark: themeColors.textDark,
+    inactiveTextDark: themeColors.inactiveTextDark,
+    iconColor: themeColors.iconColor,
+    purple: themeColors.purple,
+    blueGreen: themeColors.blueGreen,
   },
   space: {
     xxs: 2,
@@ -23,6 +36,17 @@ const theme = extendTheme({
     xl: 32,
     xxl: 40,
     xxxl: 48,
+  },
+  components: {
+    Pressable: {
+      variants: {
+        login: {
+          borderBottomWidth: 1,
+          borderBottomColor: 'cyan',
+          paddingX: 'xs',
+        },
+      },
+    },
   },
 });
 
