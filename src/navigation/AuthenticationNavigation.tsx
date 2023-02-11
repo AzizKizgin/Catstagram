d2e1c925378043c1f73742c5e987ec7edb1cb674
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-11 17:35:51 +0300

    feat: navigation screen options added

--- src/navigation/AuthenticationNavigation.tsx
+++ src/navigation/AuthenticationNavigation.tsx
@@ -1,11 +1,18 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import theme from '../../theme';
 import {ResetPassword, Login, Register} from '../screens';
 
 const AuthenticationNavigation = () => {
-  const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator<AuthNavigationParamsList>();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.itemBgDark,
+        },
+        headerTintColor: theme.colors.textDark,
+      }}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="ResetPassword" component={ResetPassword} />
