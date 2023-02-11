516ee414cf5ee7b278897a750f1d479014f9ad16
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-11 17:36:52 +0300

    feat: types file added

---
+++ src/types/types.d.ts
@@ -0,0 +1,11 @@
+type AuthNavigationParamsList = {
+  Login: undefined;
+  Register: undefined;
+  ResetPassword: undefined;
+};
+
+type MainNavigationParamsList = {
+  Home: undefined;
+  Profile: undefined;
+  Explore: undefined;
+};
