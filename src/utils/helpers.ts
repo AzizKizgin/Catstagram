b3ed1344841363d06fd163ac696d837d34679825
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-13 21:40:27 +0300

    feat: getTimeDifference function added

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -20,3 +20,28 @@ export const getImageRatio = (width?: number) => {
   const ratio = width / screenWidth;
   return ratio;
 };
+
+export const getTimeDifference = (timestamp: string) => {
+  const date = new Date(Number(timestamp));
+  const now = new Date();
+  const diff = (now.getTime() - date.getTime()) / 1000;
+
+  switch (true) {
+    case diff < 1:
+      return 'just now';
+    case diff < 60:
+      return Math.floor(diff) + 'seconds ago';
+    case diff < 3600:
+      return Math.floor(Number(diff) / 60) + ' minutes ago';
+    case diff < 86400:
+      return Math.floor(Number(diff) / 3600) + ' hours ago';
+    case diff < 604800:
+      return Math.floor(Number(diff) / 86400) + ' days ago';
+    case diff < 2592000:
+      return Math.floor(Number(diff) / 604800) + ' weeks ago';
+    case diff < 31536000:
+      return Math.floor(Number(diff) / 2592000) + ' months ago';
+    default:
+      return Math.floor(Number(diff) / 31536000) + ' years ago';
+  }
+};
