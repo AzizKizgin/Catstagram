af5c7e5acfe552d42e1ff17aaf5bfd42e88c1816
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-13 21:41:15 +0300

    feat: component updated

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,21 +1,50 @@
 import {VStack} from 'native-base';
-import React from 'react';
-import PostBottom from './components/PostBottom';
+import React, {useState, memo, FC} from 'react';
+import CommentModal from './components/modals/CommentModal';
+import PostActions from './components/PostActions';
 import PostImage from './components/PostImage';
+import PostInfo from './components/PostInfo';
 import PostUser from './components/PostUser';
-
-const Post = () => {
+const fakeComments = [
+  {
+    comment: 'Lorem ipsum dolor sit amet.',
+    user: {
+      name: 'User Name',
+      image: 'https://picsum.photos/331',
+    },
+  },
+  {
+    comment: 'Consectetur adipiscing elit.',
+    user: {
+      name: 'User Name',
+      image: 'https://picsum.photos/331',
+    },
+  },
+];
+interface PostProps {
+  post: Post;
+}
+const Post: FC<PostProps> = (props) => {
+  const {post} = props;
+  const [isCommentModalOpen, setIsCommentModalOpen] = useState(false);
+  const onCommentPress = () => {
+    setIsCommentModalOpen(true);
+  };
   return (
     <VStack space={'xs'}>
       <PostUser />
-      <PostImage
-        image={
-          'http://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg'
-        }
+      <PostImage image={post.image} />
+      <VStack space={'0.5'}>
+        <PostActions onCommentPress={onCommentPress} />
+        <PostInfo caption={post.caption} timestamp={post.createdAt} />
+      </VStack>
+      <CommentModal
+        isOpen={isCommentModalOpen}
+        setIsOpen={setIsCommentModalOpen}
+        comments={fakeComments}
       />
-      <PostBottom />
     </VStack>
   );
 };
 
-export default Post;
+export default memo(Post);
