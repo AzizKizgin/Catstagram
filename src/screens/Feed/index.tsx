ff685b506b98c9c215a7755e097475591f0822ce
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-13 21:40:51 +0300

    feat: screen updated

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,14 +1,29 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Box} from 'native-base';
+import Post from '../../components/Post';
+import {getPosts} from '../../data/getData';
+import {FlatList} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 
 const Feed = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const navigation = useNavigation();
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getPosts().then((posts) => setPosts(posts));
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>Feed</Text>
-    </View>
+    <Box flex={1} backgroundColor={'bgDark'}>
+      <FlatList
+        data={posts}
+        renderItem={({item}) => <Post post={item} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </Box>
   );
 };
 
 export default Feed;
-
-const styles = StyleSheet.create({});
