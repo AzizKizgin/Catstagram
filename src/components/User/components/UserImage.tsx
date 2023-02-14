35d335ee1d9c9d9c646e1123a7604ded22317d66
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-14 19:11:09 +0300

    feat: image component changed

--- src/components/User/components/UserImage.tsx
+++ src/components/User/components/UserImage.tsx
@@ -1,19 +1,23 @@
-import {Center, Image} from 'native-base';
+import {Center} from 'native-base';
 import React, {FC} from 'react';
+import {Image} from 'react-native';
+import {defaultProfileImage} from '../../../utils/consts';
 
 interface UserImageProps {
-  image: string;
+  image?: string;
   size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full';
 }
 const UserImage: FC<UserImageProps> = ({image, size}) => {
   return (
     <Center>
       <Image
-        source={{uri: image}}
-        alt="image base"
-        size={size}
-        rounded="full"
-        resizeMode="cover"
+        source={{
+          uri: `data:image/jpeg;base64,${image || defaultProfileImage}`,
+        }}
+        style={{
+          width: 35,
+          height: 35,
+        }}
       />
     </Center>
   );
