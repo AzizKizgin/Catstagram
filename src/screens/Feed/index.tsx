4b619618bfc3e50b1ac6ee80b08d0ac0b948e382
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-16 22:12:24 +0300

    feat: refresh added

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,18 +1,36 @@
 import React, {useEffect, useState} from 'react';
-import {Box} from 'native-base';
+import {Box, Center, Text} from 'native-base';
 import Post from '../../components/Post';
-import {FlatList} from 'react-native-gesture-handler';
+import {FlatList, RefreshControl} from 'react-native-gesture-handler';
 import {useNavigation} from '@react-navigation/native';
 import {getPosts} from '../../data/Posts/postData';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import theme from '../../../theme';
+import {ActivityIndicator} from 'react-native';
 
 const Feed = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const navigation = useNavigation();
+  const [loading, setLoading] = useState<boolean>(false);
+  const refreshData = () => {
+    getPosts().then((posts) => {
+      setPosts(posts);
+    });
+  };
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
-      getPosts().then((posts) => setPosts(posts));
+      AsyncStorage.getItem('refresh').then((value) => {
+        if (value === 'true') {
+          refreshData();
+          AsyncStorage.setItem('refresh', 'false');
+        }
+      });
     });
     return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    getPosts().then((posts) => setPosts(posts));
   }, []);
 
   return (
@@ -20,8 +38,24 @@ const Feed = () => {
       <FlatList
         data={posts}
         renderItem={({item}) => <Post post={item} />}
+        ListEmptyComponent={
+          <Center flex={1}>
+            <ActivityIndicator size="large" color={theme.colors.cyan} />
+          </Center>
+        }
         keyExtractor={(item, index) => index.toString()}
         ListFooterComponent={<Box height={50} />}
+        refreshControl={
+          <RefreshControl
+            colors={[theme.colors.cyan]}
+            refreshing={loading}
+            onRefresh={async () => {
+              setLoading(true);
+              refreshData();
+              setLoading(false);
+            }}
+          />
+        }
       />
     </Box>
   );
