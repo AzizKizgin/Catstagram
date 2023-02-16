2c23b668deb6d61e8eb52dec682e770a6fe73568
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-16 19:37:48 +0300

    feat: post context added

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,7 +1,6 @@
-import {useNavigation} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {VStack} from 'native-base';
-import React, {useState, memo, FC} from 'react';
+import React, {memo, FC} from 'react';
+import {PostProvider} from '../../context/PostContext';
 import PostActions from './components/PostActions';
 import PostImage from './components/PostImage';
 import PostInfo from './components/PostInfo';
@@ -12,20 +11,17 @@ interface PostProps {
 }
 const Post: FC<PostProps> = (props) => {
   const {post} = props;
-  const navigation =
-    useNavigation<NativeStackNavigationProp<FeedNavigationParamsList>>();
-  const onCommentPress = () => {
-    navigation.navigate('Comments', {postId: post.id});
-  };
   return (
-    <VStack space={'xs'} marginTop={'sm'}>
-      <PostUser userId={post.userId} />
-      <PostImage image={post.image} />
-      <VStack>
-        <PostActions onCommentPress={onCommentPress} postId={post.id} />
-        <PostInfo caption={post.caption} timestamp={post.createdAt} />
+    <PostProvider post={post}>
+      <VStack space={'xs'} marginTop={'sm'}>
+        <PostUser userId={post.userId} />
+        <PostImage image={post.image} />
+        <VStack>
+          <PostActions />
+          <PostInfo caption={post.caption} timestamp={post.createdAt} />
+        </VStack>
       </VStack>
-    </VStack>
+    </PostProvider>
   );
 };
 
