972e60be7f40975c85f657f353253402f7c34146
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-16 22:53:27 +0300

    feat: double tap like added

--- src/components/Post/components/PostImage.tsx
+++ src/components/Post/components/PostImage.tsx
@@ -1,26 +1,68 @@
 import React, {FC, memo} from 'react';
-import {Box} from 'native-base';
-import {getImageHeight} from '../../../utils/helpers';
+import {Box, Center, Icon, Pressable} from 'native-base';
 import {Image} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import {
+  GestureHandlerRootView,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import AnimatedCenter from '../../AnimatedComponents/AnimatedCenter';
+import AnimatedBox from '../../AnimatedComponents/AnimatedBox';
+import {withDelay, withSpring, withTiming} from 'react-native-reanimated';
+import {usePost} from '../../../context/PostContext';
 
 interface Props {
   image: PostImage;
 }
-
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 const PostImage: FC<Props> = ({image}) => {
+  const {like, isUserLiked} = usePost();
+  const scale = useSharedValue(0);
+  const scaleStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: scale.value}],
+    };
+  });
+
+  const doubleTap = () => {
+    if (!isUserLiked) {
+      like();
+    }
+    console.log('first');
+
+    scale.value = withSpring(1, undefined, (isFinished) => {
+      if (isFinished) {
+        scale.value = withDelay(100, withTiming(0));
+      }
+    });
+  };
   return (
-    <Box>
-      <Image
-        source={{
-          uri: `data:image/jpeg;base64,${image.imageUri}`,
-        }}
-        style={{
-          width: '100%',
-          height: image.height,
-        }}
-        resizeMode={'cover'}
-      />
-    </Box>
+    <GestureHandlerRootView>
+      <TapGestureHandler
+        onActivated={doubleTap}
+        numberOfTaps={2}
+        maxDurationMs={200}>
+        <AnimatedBox justifyContent="center" alignItems="center">
+          <AnimatedImage
+            source={{
+              uri: `data:image/jpeg;base64,${image.imageUri}`,
+            }}
+            style={{
+              width: '100%',
+              height: image.height,
+            }}
+            resizeMode={'cover'}
+          />
+          <AnimatedCenter position="absolute" style={scaleStyle}>
+            <Icon as={AntDesign} name="like1" size={125} color="iconColor" />
+          </AnimatedCenter>
+        </AnimatedBox>
+      </TapGestureHandler>
+    </GestureHandlerRootView>
   );
 };
 
