895b58a8148b048447b22dc85789d152e6068f14
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-16 19:35:53 +0300

    feat: component added

---
+++ src/components/AnimatedComponents/AnimatedCenter.tsx
@@ -0,0 +1,5 @@
+import {Center} from 'native-base';
+import Animated from 'react-native-reanimated';
+
+const AnimateCenter = Animated.createAnimatedComponent(Center);
+export default AnimateCenter;
