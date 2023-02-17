5e351203849789acec449160a3337239d17fa4e0
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-17 23:56:40 +0300

    feat: post detail added

--- src/screens/Account/components/UserPosts.tsx
+++ src/screens/Account/components/UserPosts.tsx
@@ -1,16 +1,24 @@
 import React, {FC, memo} from 'react';
 import {Box, HStack, Pressable} from 'native-base';
 import {Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 interface UserPostsProps {
   posts: Post[];
 }
 const UserPosts: FC<UserPostsProps> = (props) => {
   const {posts} = props;
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AccountNavigationParamsList>>();
+
+  const onPress = (post: Post) => {
+    navigation.navigate('PostDetail', {post});
+  };
   return (
     <HStack marginTop={'l'} flexWrap={'wrap'}>
       {posts.map((post) => (
-        <Pressable key={post.id} onPress={() => {}}>
+        <Pressable key={post.id} onPress={() => onPress(post)}>
           <Box marginTop={'xxs'} marginX={'xxs'}>
             <Image
               source={{
