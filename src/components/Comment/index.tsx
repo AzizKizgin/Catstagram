cb2b5afba126a54dd4021c5c3833a3aaa2332377
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-17 19:43:36 +0300

    feat: UserImage updated

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -24,7 +24,7 @@ const Comment: FC<CommentProps> = (props) => {
       paddingX={'m'}
       space={'4'}>
       <HStack space={'4'} alignItems={'center'} flex={1}>
-        <UserImage image={defaultProfileImage} size={'small'} />
+        <UserImage image={defaultProfileImage} size={'small'} userId={userId} />
         <Box flex={1}>
           <HStack space={'2'}>
             <Text color={'gray.400'} textAlign={'left'}>
--- src/components/User/components/UserImage.tsx
+++ src/components/User/components/UserImage.tsx
@@ -1,26 +1,38 @@
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {Center} from 'native-base';
 import React, {FC} from 'react';
-import {Image} from 'react-native';
+import {Image, Pressable} from 'react-native';
 import {defaultProfileImage} from '../../../utils/consts';
 
 interface UserImageProps {
   image?: string | null;
   size: 'small' | 'large';
+  userId?: string;
 }
-const UserImage: FC<UserImageProps> = ({image, size}) => {
+const UserImage: FC<UserImageProps> = ({image, size, userId}) => {
   const imageSize = size === 'small' ? 35 : 80;
+  const navigation =
+    useNavigation<NativeStackNavigationProp<FeedNavigationParamsList>>();
+  const onPress = () => {
+    if (userId) {
+      navigation.navigate('UserAccount', {userId});
+    }
+  };
   return (
-    <Center>
-      <Image
-        source={{
-          uri: `data:image/jpeg;base64,${image || defaultProfileImage}`,
-        }}
-        style={{
-          width: imageSize,
-          height: imageSize,
-        }}
-      />
-    </Center>
+    <Pressable onPress={onPress}>
+      <Center>
+        <Image
+          source={{
+            uri: `data:image/jpeg;base64,${image || defaultProfileImage}`,
+          }}
+          style={{
+            width: imageSize,
+            height: imageSize,
+          }}
+        />
+      </Center>
+    </Pressable>
   );
 };
 
