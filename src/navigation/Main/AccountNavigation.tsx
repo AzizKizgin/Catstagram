fc2bbb5ccaff0965a3d22fc2b3a24a75f7ab35c5
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-19 21:42:42 +0300

    feat: Account page header added

--- src/navigation/Main/AccountNavigation.tsx
+++ src/navigation/Main/AccountNavigation.tsx
@@ -1,6 +1,7 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {Account, PostDetail} from '../../screens';
+import AccountHeader from '../components/AccountHeader';
 
 const AccountNavigation = () => {
   const Stack = createNativeStackNavigator();
@@ -8,7 +9,14 @@ const AccountNavigation = () => {
     <Stack.Navigator
       screenOptions={{
         animation: 'fade_from_bottom',
-        headerShown: false,
+        headerShown: true,
+        header(props) {
+          if (props.route.name === 'Account') {
+            return <AccountHeader props={props} />;
+          } else {
+            return null;
+          }
+        },
       }}>
       <Stack.Screen name="Account" component={Account} />
       <Stack.Screen name="PostDetail" component={PostDetail} />
---
+++ src/navigation/components/AccountHeader.tsx
@@ -0,0 +1,32 @@
+import React, {FC} from 'react';
+import {Box, HStack} from 'native-base';
+import HeaderLogo from '../../components/Shared/HeaderLogo';
+import AddPostButton from '../../components/Shared/AddPostButton';
+import {NativeStackHeaderProps} from '@react-navigation/native-stack';
+import MenuButton from '../../components/Shared/MenuButton';
+
+interface AccountHeaderProps {
+  props: NativeStackHeaderProps;
+}
+const AccountHeader: FC<AccountHeaderProps> = ({props}) => {
+  return (
+    <Box
+      paddingX={'sm'}
+      backgroundColor={'bgDark'}
+      flexDirection={'row'}
+      alignItems={'center'}
+      justifyContent={'space-between'}>
+      <HeaderLogo />
+      <HStack space={1}>
+        <AddPostButton
+          onPress={() => {
+            props.navigation.navigate('AddPost');
+          }}
+        />
+        <MenuButton />
+      </HStack>
+    </Box>
+  );
+};
+
+export default AccountHeader;
