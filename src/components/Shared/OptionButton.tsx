b39503211d61bb5e746b934b46d3554ca5a0d561
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-20 13:32:43 +0300

    feat: button updated

--- src/components/Shared/OptionButton.tsx
+++ src/components/Shared/OptionButton.tsx
@@ -1,20 +1,61 @@
-import {Box, Icon} from 'native-base';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {Box, Icon, Menu, Pressable} from 'native-base';
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import AnimatedPressable from '../AnimatedComponents/AnimatedPressable';
+import theme from '../../../theme';
+import {useAuth} from '../../context/AuthContext';
+import {usePost} from '../../context/PostContext';
+import {deletePost} from '../../data/Posts/postData';
 
 const OptionButton = () => {
+  const {user} = useAuth();
+  const {post} = usePost();
+  const route = useRoute();
+  const navigation = useNavigation();
   return (
-    <AnimatedPressable>
-      <Box>
-        <Icon
-          as={MaterialCommunityIcons}
-          name="dots-vertical"
-          size="lg"
-          color="iconColor"
-        />
-      </Box>
-    </AnimatedPressable>
+    <Box alignItems="center">
+      <Menu
+        w="190"
+        backgroundColor={'bgDark'}
+        borderWidth={0.5}
+        borderColor={'iconColor'}
+        trigger={(triggerProps) => {
+          return (
+            <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+              <Icon
+                as={MaterialCommunityIcons}
+                name="dots-vertical"
+                size="lg"
+                color="iconColor"
+              />
+            </Pressable>
+          );
+        }}>
+        <Menu.Item _text={{color: 'textDark', fontSize: 'sm'}}>
+          Download Image
+        </Menu.Item>
+        <Menu.Item
+          _text={{color: 'textDark', fontSize: 'sm'}}
+          display={
+            post?.userId === user?.uid && route.name !== 'Feed'
+              ? 'flex'
+              : 'none'
+          }
+          onPress={() => {
+            deletePost(post?.id, user?.uid);
+            navigation.getParent()?.setOptions({
+              tabBarStyle: {
+                backgroundColor: theme.colors.itemBgDark,
+                borderTopColor: theme.colors.itemBgDark,
+                height: 50,
+              },
+            });
+            navigation.goBack();
+          }}>
+          Delete
+        </Menu.Item>
+      </Menu>
+    </Box>
   );
 };
 
