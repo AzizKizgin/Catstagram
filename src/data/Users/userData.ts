a2f5d65a08b13905db5533a59fb78dd2ac39d989
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-22 17:42:11 +0300

    feat: user data updated

--- src/data/Users/userData.ts
+++ src/data/Users/userData.ts
@@ -1,5 +1,6 @@
 import firestore from '@react-native-firebase/firestore';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
+import {sendNotification} from '../../utils/helpers';
 import {getPostById} from '../Posts/postData';
 
 export const addUserInfo = async (user: User) => {
@@ -59,7 +60,7 @@ export const getUserPosts = async (userId?: string) => {
   const allPosts: Post[] = [];
   if (userId) {
     let allPostIds: string[] = [];
-    const allPostsDocs = await firestore()
+    await firestore()
       .collection('users')
       .doc(userId)
       .collection('activities')
@@ -102,4 +103,291 @@ export const updateUserInfo = async (
       bio: userBio,
     });
   }
+};
+
+export const getUserLikedPosts = async (userId?: string) => {
+  const allLikedPosts: Post[] = [];
+  if (userId) {
+    let allLikedPostsIds: string[] = [];
+    const allLikedPostsDocs = await firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('likes')
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          allLikedPostsIds = doc.data()?.likes;
+        }
+      });
+    if (allLikedPostsIds) {
+      for (let i = 0; i < allLikedPostsIds.length; i++) {
+        const post = await getPostById(allLikedPostsIds[i]);
+        if (post) {
+          allLikedPosts.push(post);
+        }
+      }
+    }
+    allLikedPosts.sort((a, b) => {
+      return Number(b.createdAt) - Number(a.createdAt);
+    });
+    return allLikedPosts;
+  }
+  return [];
+};
+
+export const fallowUser = async (
+  userId?: string,
+  userName?: string,
+  followId?: string,
+  fallowDeviceToken?: string,
+) => {
+  if (userId && followId) {
+    const fallowDoc = firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('follows');
+
+    fallowDoc
+      .get()
+      .then(async (doc) => {
+        if (doc.exists) {
+          if (!doc.data()?.follows?.includes(followId)) {
+            await fallowDoc.update({
+              follows: firestore.FieldValue.arrayUnion(followId),
+            });
+          }
+        } else {
+          await fallowDoc.set({
+            follows: firestore.FieldValue.arrayUnion(followId),
+          });
+        }
+      })
+      .then(async () => {
+        const targetUserFallowRequestDoc = firestore()
+          .collection('users')
+          .doc(followId)
+          .collection('activities')
+          .doc('fallowRequests');
+        targetUserFallowRequestDoc
+          .get()
+          .then(async (doc) => {
+            if (doc.exists) {
+              if (!doc.data()?.fallowRequests?.includes(userId)) {
+                await targetUserFallowRequestDoc.update({
+                  fallowRequests: firestore.FieldValue.arrayUnion(userId),
+                });
+              }
+            } else {
+              await targetUserFallowRequestDoc.set({
+                fallowRequests: firestore.FieldValue.arrayUnion(userId),
+              });
+            }
+          })
+          .then(async () => {
+            if (fallowDeviceToken) {
+              sendNotification(
+                fallowDeviceToken,
+                'New Fallow',
+                `${userName} just fallowed you`,
+              );
+            }
+          });
+      });
+  }
+};
+
+export const unfallowUser = async (userId?: string, followId?: string) => {
+  if (userId && followId) {
+    const fallowDoc = firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('follows');
+
+    fallowDoc.get().then(async (doc) => {
+      if (doc.exists) {
+        if (doc.data()?.follows?.includes(followId)) {
+          await fallowDoc.update({
+            follows: firestore.FieldValue.arrayRemove(followId),
+          });
+        }
+      }
+    });
+  }
+};
+
+export const getUserFallowers = async (userId?: string) => {
+  const allFallowers: User[] = [];
+  if (userId) {
+    let allFallowersIds: string[] = [];
+    const allFallowersDocs = await firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('fallowers')
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          allFallowersIds = doc.data()?.fallowers;
+        }
+      });
+    if (allFallowersIds) {
+      for (let i = 0; i < allFallowersIds.length; i++) {
+        const user = await getUserById(allFallowersIds[i]);
+        if (user) {
+          allFallowers.push(user);
+        }
+      }
+    }
+    return allFallowers;
+  }
+  return [];
+};
+
+export const getUserFallowing = async (userId?: string) => {
+  const allFallowing: User[] = [];
+  if (userId) {
+    let allFallowingIds: string[] = [];
+    const allFallowingDocs = await firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('follows')
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          allFallowingIds = doc.data()?.follows;
+        }
+      });
+    if (allFallowingIds) {
+      for (let i = 0; i < allFallowingIds.length; i++) {
+        const user = await getUserById(allFallowingIds[i]);
+        if (user) {
+          allFallowing.push(user);
+        }
+      }
+    }
+    return allFallowing;
+  }
+  return [];
+};
+
+export const isUserFallowingTarget = async (
+  userId?: string,
+  followId?: string,
+) => {
+  let isFallowing = false;
+  if (userId && followId) {
+    const fallowDoc = await firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('follows')
+      .get();
+    if (fallowDoc.exists) {
+      if (fallowDoc.data()?.follows?.includes(followId)) {
+        isFallowing = true;
+      } else {
+        isFallowing = false;
+      }
+    }
+  }
+  return isFallowing;
+};
+
+export const isTargetFallowingUser = async (
+  userId?: string,
+  followId?: string,
+) => {
+  const [isFallowing, setIsFallowing] = useState<boolean>(false);
+  if (userId && followId) {
+    const fallowDoc = await firestore()
+      .collection('users')
+      .doc(followId)
+      .collection('activities')
+      .doc('follows')
+      .get();
+    if (fallowDoc.exists) {
+      if (fallowDoc.data()?.follows?.includes(userId)) {
+        setIsFallowing(true);
+      } else {
+        setIsFallowing(false);
+      }
+    }
+  }
+  return isFallowing;
+};
+
+export const getFallowRequests = async (userId?: string) => {
+  const allFallowRequests: User[] = [];
+  if (userId) {
+    let allFallowRequestsIds: string[] = [];
+    const allFallowRequestsDocs = await firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('fallowRequests')
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          allFallowRequestsIds = doc.data()?.fallowRequests;
+        }
+      });
+    if (allFallowRequestsIds) {
+      for (let i = 0; i < allFallowRequestsIds.length; i++) {
+        const user = await getUserById(allFallowRequestsIds[i]);
+        if (user) {
+          allFallowRequests.push(user);
+        }
+      }
+    }
+    return allFallowRequests;
+  }
+  return [];
+};
+
+export const acceptFallowRequest = async (
+  userId?: string,
+  userName?: string,
+  followId?: string,
+  fallowDeviceToken?: string,
+) => {
+  fallowUser(userId, userName, followId, fallowDeviceToken).then(() => {
+    const fallowRequestDoc = firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('activities')
+      .doc('fallowRequests');
+    fallowRequestDoc.get().then(async (doc) => {
+      if (doc.exists) {
+        if (doc.data()?.fallowRequests?.includes(followId)) {
+          await fallowRequestDoc.update({
+            fallowRequests: firestore.FieldValue.arrayRemove(followId),
+          });
+        }
+      }
+    });
+  });
+};
+
+export const declineFallowRequest = async (
+  userId?: string,
+  followId?: string,
+) => {
+  const fallowRequestDoc = firestore()
+    .collection('users')
+    .doc(userId)
+    .collection('activities')
+    .doc('fallowRequests');
+  fallowRequestDoc.get().then(async (doc) => {
+    if (doc.exists) {
+      if (doc.data()?.fallowRequests?.includes(followId)) {
+        await fallowRequestDoc.update({
+          fallowRequests: firestore.FieldValue.arrayRemove(followId),
+        });
+      }
+    }
+  });
 };
--- src/screens/Account/components/ActivityButtons.tsx
+++ src/screens/Account/components/ActivityButtons.tsx
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {HStack, Button} from 'native-base';
+import {
+  fallowUser,
+  isTargetFallowingUser,
+  isUserFallowingTarget,
+  unfallowUser,
+} from '../../../data/Users/userData';
+import {useAuth} from '../../../context/AuthContext';
+import {Alert} from 'react-native';
 
-const ActivityButtons = () => {
+interface ActivityButtonsProps {
+  userId: string;
+  userToken?: string;
+}
+
+const ActivityButtons: FC<ActivityButtonsProps> = ({userId, userToken}) => {
+  const [isFallowing, setIsFallowing] = useState<boolean>(false);
+  const [isTargetFallowing, setIsTargetFallowing] = useState<boolean>(false);
+  const {user: appUser} = useAuth();
+  const unFallowUser = () => {
+    Alert.alert(
+      'Unfallow User',
+      'Are you sure you want to unfallow this user?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: () => {
+            unfallowUser(appUser?.uid, userId).then(() => {
+              setIsFallowing(false);
+            });
+          },
+        },
+      ],
+    );
+  };
+
+  useEffect(() => {
+    isUserFallowingTarget(appUser?.uid, userId).then((isFallowing) => {
+      setIsFallowing(isFallowing);
+    });
+    isTargetFallowingUser(appUser?.uid, userId).then((isFallowing) => {
+      setIsTargetFallowing(isFallowing);
+    });
+  }, []);
   return (
     <HStack justifyContent={'space-evenly'} marginTop={'sm'}>
-      <Button width={'45%'} color={'cyan'}>
-        Fallow
-      </Button>
+      {isFallowing ? (
+        <Button
+          width={'45%'}
+          color={'cyan'}
+          variant={'outline'}
+          onPress={unFallowUser}>
+          Fallowing
+        </Button>
+      ) : (
+        <Button
+          width={'45%'}
+          color={'cyan'}
+          onPress={() => {
+            fallowUser(
+              appUser?.uid,
+              appUser?.displayName || '',
+              userId,
+              userToken || '',
+            ).then(() => {
+              setIsFallowing(true);
+            });
+          }}>
+          Fallow
+        </Button>
+      )}
       <Button width={'45%'} color={'cyan'}>
         Message
       </Button>
