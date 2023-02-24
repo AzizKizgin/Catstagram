b05488fe2aaf2ff6917c867a543e6e924a6be9f3
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-25 01:47:04 +0300

    feat: props updated

--- src/components/Shared/CommentLikeButton.tsx
+++ src/components/Shared/CommentLikeButton.tsx
@@ -1,24 +1,23 @@
-import {Box, Icon} from 'native-base';
+import {Box, Icon, Text} from 'native-base';
 import React, {FC, memo, useEffect, useState} from 'react';
 import AnimatedPressable from '../AnimatedComponents/AnimatedPressable';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {ThemeComponentSizeType} from 'native-base/lib/typescript/components/types';
+import {useAuth} from '../../context/AuthContext';
 interface LikeButtonProps {
   onPress?: () => void;
   size?: ThemeComponentSizeType<'Icon'>;
-  id?: string;
-  isUserLiked?: boolean;
+  likes?: string[];
 }
 const CommentLikeButton: FC<LikeButtonProps> = (props) => {
-  const {size, onPress, id, isUserLiked = false} = props;
-  const [isLiked, setIsLiked] = useState(isUserLiked);
-  useEffect(() => {
-    setIsLiked(isUserLiked);
-  }, [isUserLiked]);
-
+  const {size, onPress, likes} = props;
+  const {user} = useAuth();
   return (
-    <AnimatedPressable onPress={onPress}>
-      {isLiked ? (
+    <AnimatedPressable
+      onPress={onPress}
+      alignItems={'center'}
+      justifyContent={'center'}>
+      {user?.uid && likes?.includes(user?.uid) ? (
         <Box>
           <Icon as={AntDesign} name="like1" size={size} color="cyan" />
         </Box>
@@ -27,6 +26,9 @@ const CommentLikeButton: FC<LikeButtonProps> = (props) => {
           <Icon as={AntDesign} name="like2" size={size} color="iconColor" />
         </Box>
       )}
+      <Text color={'gray.400'}>
+        {likes && likes?.length > 0 && likes?.length}
+      </Text>
     </AnimatedPressable>
   );
 };
