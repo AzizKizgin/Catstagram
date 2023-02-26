a597bb5721ed89c2c5176493e832adfb89696355
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-26 18:15:54 +0300

    feat: component changed

--- src/components/Shared/PostLikeButton.tsx
+++ src/components/Shared/PostLikeButton.tsx
@@ -1,15 +1,13 @@
-import {Box, Icon} from 'native-base';
+import {Box, Icon, Pressable} from 'native-base';
 import React, {memo, useState} from 'react';
-import AnimatedPressable from '../AnimatedComponents/AnimatedPressable';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import {useAuth} from '../../context/AuthContext';
 import {usePost} from '../../context/PostContext';
 
 const PostLikeButton = () => {
-  const {post, like, isUserLiked} = usePost();
+  const {like, isUserLiked} = usePost();
 
   return (
-    <AnimatedPressable onPress={like}>
+    <Pressable onPress={like}>
       {isUserLiked ? (
         <Box>
           <Icon as={AntDesign} name="like1" size={23} color="cyan" />
@@ -19,7 +17,7 @@ const PostLikeButton = () => {
           <Icon as={AntDesign} name="like2" size={23} color="iconColor" />
         </Box>
       )}
-    </AnimatedPressable>
+    </Pressable>
   );
 };
 
